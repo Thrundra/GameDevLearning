6726d8582677b2f1206aee2042f3b20763f333b5
Brett Gilbert <brett@kalain.co.uk>
2018-07-08 10:33:54 +0100

    Initial Push to github
    This is the initial push to github.
    
    I won't be using VSTS for any Unity work since VSTS doesn't have a standard template for Unity.

---
+++ Brett's Self Learning/Assets/Game Types/Full Mouse Move/Scripts/Ship.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+    // COnfig data
+    [SerializeField] float screenWidthInUnits = 18f;
+    [SerializeField] float screenHeightInUnitys = 10f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 17f;
+    [SerializeField] float minY = 1f;
+    [SerializeField] float maxY = 9f;
+
+    float mousePosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mouseXPos = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        float mouseYPos = Input.mousePosition.y / Screen.height * screenHeightInUnitys;
+
+        Vector2 shipPosition = new Vector2(transform.position.x, transform.position.y);
+        shipPosition.x = Mathf.Clamp(mouseXPos, minX, maxX);
+        shipPosition.y = Mathf.Clamp(mouseYPos, minY, maxY);
+
+        transform.position = shipPosition;
+	}
+}
---
+++ Brett's Self Learning/Assets/Game Types/Waypoints/Scripts/NewWaypoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewWaypoint : MonoBehaviour {
+
+    // Initialise everything
+    [SerializeField] public Transform[] wayPointList;
+
+    public int currentWaypoint = 0;
+    Transform targetWayPoint;
+
+    public float spriteSpeed = 4f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(currentWaypoint < this.wayPointList.Length)
+        {
+            if (targetWayPoint == null)
+                targetWayPoint = wayPointList[currentWaypoint];
+            MoveToWayPoint();
+        }
+	}
+
+    void MoveToWayPoint()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position, spriteSpeed * Time.deltaTime);
+
+        if(transform.position == targetWayPoint.position)
+        {
+            currentWaypoint++;
+            targetWayPoint = wayPointList[currentWaypoint];
+        }
+    }
+}
