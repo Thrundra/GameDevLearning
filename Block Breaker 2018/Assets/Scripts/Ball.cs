d88ecc40d70987c27f90fe766c5fff49947cb2a6
Brett Gilbert <brett@kalain.co.uk>
2018-07-10 21:27:36 +0100

    Loads of work on BlockBreaker 2018
    Physics and moving the ball and bat.

---
+++ Block Breaker 2018/Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    // Config parameters
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float velocityX = 2f;
+    [SerializeField] float velocityY = 15f;
+
+    // state
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = transform.position - paddle1.transform.position; 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+
+	}
+
+    private void LaunchOnMouseClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX, velocityY);
+            hasStarted = true;
+        }
+    }
+
+    void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
