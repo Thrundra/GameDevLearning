76e282edea301c5822b0f78e878d37035232ccff
Brett Gilbert <brett@kalain.co.uk>
2018-07-19 21:32:52 +0100

    g
    g

---
+++ Block Breaker 2018/Assets/Scripts/Block.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    // Script Config Information
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
