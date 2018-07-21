d6bc4f93ad6df71c47492d2da08f6cd3abc1aa4e
Brett Gilbert <brett@kalain.co.uk>
2018-07-21 11:11:13 +0100

    more work
    more 2018 block buster and space invaders.

---
+++ Laser Defender Old/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    // Configuration
+
+    [SerializeField] float speed = 15f;
+
+    float padding = 1f;
+    float xMin = -5f;
+    float xMax = 5f;
+
+    private void Start()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftMostPosition = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+        Vector3 rightMostPosition = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xMin = leftMostPosition.x + padding;
+        xMax = rightMostPosition.x - padding;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector2 shipPos = new Vector2(transform.position.x, transform.position.y);
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        // Restricting player to the game space.
+        float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}
