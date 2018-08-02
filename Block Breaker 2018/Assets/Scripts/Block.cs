d2e53faa20af771a3be171d56f398cb17537dcf0
Brett Gilbert <brett@kalain.co.uk>
2018-08-02 07:59:45 +0100

    adding particles
    Adding particles

--- Block Breaker 2018/Assets/Scripts/Block.cs
+++ Block Breaker 2018/Assets/Scripts/Block.cs
@@ -6,6 +6,7 @@ public class Block : MonoBehaviour {
 
     // Script Config Information
     [SerializeField] AudioClip audioClip;
+    [SerializeField] GameObject blockSparklesVFX;
 
     // Cached Reference
     LevelManager level;
@@ -25,8 +26,20 @@ public class Block : MonoBehaviour {
     private void DestroyBlock()
     {
         level.BlockDestroyed();
+        PlayBlockDestroySFX();
+        TriggerSparklesVFX();
+        Destroy(gameObject);
+    }
+
+    private void PlayBlockDestroySFX()
+    {
         FindObjectOfType<GameStatus>().AddToScore();
         AudioSource.PlayClipAtPoint(audioClip, Camera.main.transform.position);
-        Destroy(gameObject);
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
     }
 }
