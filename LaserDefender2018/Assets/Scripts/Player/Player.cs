73e703e3c8b84331e9b14d6f686b65db5bc764b2
Brett Gilbert <brett@kalain.co.uk>
2018-08-18 19:16:53 +0100

    laser defender 2018
    Starting Laser defender 2018

---
+++ LaserDefender2018/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float playerMoveSpeed = 10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MovePlayer();
+	}
+
+    private void MovePlayer()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * playerMoveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * playerMoveSpeed;
+        var newXPos = transform.position.x + deltaX;
+        var newYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
