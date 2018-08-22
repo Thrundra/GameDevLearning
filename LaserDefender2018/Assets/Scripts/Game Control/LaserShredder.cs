9eb1aa0262592e2dc76c5920f21308821f20f9a9
Brett Gilbert <brett@kalain.co.uk>
2018-08-22 20:13:11 +0100

    Laser Defence
    Waypoints and Movement

--- LaserDefender2018/Assets/Scripts/Game Control/LaserShredder.cs
+++ LaserDefender2018/Assets/Scripts/Game Control/LaserShredder.cs
@@ -6,7 +6,6 @@ public class LaserShredder : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log("I've been hit by a " + collider.gameObject.name);
         Destroy(collider.gameObject);
     }
 }
