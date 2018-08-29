a11ffa484cf4c205d850ad3459e29380aa50f39e
Brett Gilbert <brett@kalain.co.uk>
2018-08-29 22:05:03 +0100

    LASER DEfender updates
    more work on LD2018

---
+++ LaserDefender2018/Assets/Scripts/Game Control/Enemy.cs
@@ -0,0 +1,58 @@
+﻿//using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float health = 100f;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+
+    [SerializeField] GameObject enemyLaser;
+    [SerializeField] float enemyLaserSpeed = -10f;
+
+	// Use this for initialization
+	void Start () {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CountDownAndShoot();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if(shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(enemyLaser, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -enemyLaserSpeed);
+    }
+}
