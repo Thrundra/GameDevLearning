31904ed26a5941882e05e0192b1fa2a963f3f553
Brett Gilbert <brett@kalain.co.uk>
2018-09-06 21:36:59 +0100

    LaserDefender2018
    Moremoenu

---
+++ LaserDefender2018/Assets/Scripts/Game Control/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGameMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
