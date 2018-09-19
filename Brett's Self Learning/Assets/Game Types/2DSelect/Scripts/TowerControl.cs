b33526e16c62a97d978b1a3234a81b68fdba217a
Brett Gilbert <brett@kalain.co.uk>
2018-09-19 21:28:45 +0100

    MyDev
    trying to create a gui which appears when you click an tower point..

---
+++ Brett's Self Learning/Assets/Game Types/2DSelect/Scripts/TowerControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerControl : MonoBehaviour
+{
+    [SerializeField] Sprite[] sprites;
+    private SpriteRenderer towerSR;
+    private Image displayImage;
+
+    // Use this for initialization
+    void Start()
+    {
+        towerSR = gameObject.GetComponent<SpriteRenderer>();
+       /// displayImage = FindObjectOfType<Image>().
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        if(towerSR.sprite.Equals(sprites[0]))
+        {
+            towerSR.sprite = sprites[1];
+        }
+        else
+        {
+            towerSR.sprite = sprites[0];
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+}
