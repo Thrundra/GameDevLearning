e588f8bd38b0c9029f70d46f23f5c15d25cfefc7
Brett Gilbert <brett@kalain.co.uk>
2018-10-24 21:55:39 +0100

    cv
    cvb

--- TDLearn/Assets/Scripts/waypointWalk.cs
+++ TDLearn/Assets/Scripts/waypointWalk.cs
@@ -6,9 +6,11 @@ public class waypointWalk : MonoBehaviour {
 
     [SerializeField] GameObject waypoints;
 
+    private Transform[] waypointLocations;
+
 	// Use this for initialization
 	void Start () {
-		
+		foreach(GameObject wpObject in waypoints)
 	}
 	
 	// Update is called once per frame
