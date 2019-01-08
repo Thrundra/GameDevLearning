47a3440c51ac001228b272adec0eb69bb4007e5e
Brett Gilbert <brett@kalain.co.uk>
2019-01-08 21:49:39 +0000

    Laser Defender mk2
    Adding background to menu and getting it to rotate as a skybox.

---
+++ Laser Defender Mk2/Assets/Scripts/Control/RotateSkyBox.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSkyBox : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 4.5f;
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed);
+    }
+}
