9e7fcd56ceece00b202a498c2e30345037af4149
Brett Gilbert <brett@kalain.co.uk>
2019-01-10 22:11:02 +0000

    My game
    trying to add damage to the ship

---
+++ Laser Defender Mk2/Assets/Scripts/Player/PlayerDamageLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamageLevel : MonoBehaviour
+{
+    [SerializeField] Sprite[] m_PlayerDamageLevels;
+    [SerializeField] GameObject parent;
+    float Fixescale = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(Fixescale / parent.transform.localScale.x, Fixescale / parent.transform.localScale.y, 0);
+    }
+}
--- Laser Defender Mk2/Assets/Scripts/Player/PlayerScript.cs
+++ Laser Defender Mk2/Assets/Scripts/Player/PlayerScript.cs
@@ -8,9 +8,19 @@ public class PlayerScript : MonoBehaviour
     // Config Parameters
     [Header("Player")]
     [SerializeField] float m_PlayerMoveSpeed = 10f;
-    [SerializeField] float m_Padding = 2f;
+    [SerializeField] float m_Padding = 0.5f;
+    [SerializeField] int m_PlayerHealth = 500;
+    [SerializeField] int m_PlayerShieldHealth = 0;
+
+    [Header("Player Weapons")]
+    [SerializeField] GameObject m_Playerlaser;
+    [SerializeField] float m_PlayerProjectileSpeed = 10f;
+    [SerializeField] float m_PlayerProjectileFiringSpeed = 0.1f;
 
     float xMin, xMax, yMin, yMax;
+    private int m_PlayerDamageLevel;
+
+    Coroutine c_PlayerFiringCoroutine;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +33,7 @@ public class PlayerScript : MonoBehaviour
         MovePlayer();
     }
 
+    #region Player Movment
     private void SetupMoveBoundaries()
     {
         Camera _gameCamera = Camera.main;
@@ -30,7 +41,7 @@ public class PlayerScript : MonoBehaviour
         xMax = _gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - m_Padding;
 
         yMin = _gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + m_Padding;
-        yMax = _gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - m_Padding;
+        yMax = _gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - m_Padding - 0.5f;
     }
 
     private void MovePlayer()
@@ -41,5 +52,15 @@ public class PlayerScript : MonoBehaviour
         var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
         var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
+
+        
     }
+    #endregion
+
+    #region Player Health
+    public int GetHealth()
+    {
+        return m_PlayerHealth;
+    }
+    #endregion
 }
