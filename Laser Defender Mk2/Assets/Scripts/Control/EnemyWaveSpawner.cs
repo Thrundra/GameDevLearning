c9a09a16a566d4234ad14312ed114c4e5fc3b54b
Brett Gilbert <brett@kalain.co.uk>
2019-01-24 21:45:45 +0000

    Enemy waves
    Starting enemy waves with waypoints.
    
    need to do shooting now

---
+++ Laser Defender Mk2/Assets/Scripts/Control/EnemyWaveSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWaveSpawner : MonoBehaviour
+{
+    [SerializeField] List<EnemyWaveConfig> m_WaveList;
+    [SerializeField] int m_StartingWaveIndex = 0;
+    [SerializeField] bool b_isLooping = false;
+
+    IEnumerator Start()
+    {
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        }
+        while (b_isLooping);
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for(int t_waveIndex = m_StartingWaveIndex; t_waveIndex < m_WaveList.Count; t_waveIndex++)
+        {
+            var m_currentWave = m_WaveList[t_waveIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(m_currentWave));
+        }
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(EnemyWaveConfig m_currentWaveConfig)
+    {
+        for(int enemyCount = 0; enemyCount < m_currentWaveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            var m_newEnemy = Instantiate(m_currentWaveConfig.GetEnemyPrefab(), m_currentWaveConfig.GetWaypointList()[0].transform.position, Quaternion.identity);
+
+            m_newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(m_currentWaveConfig);
+
+            yield return new WaitForSeconds(m_currentWaveConfig.GetTimeBetweenSpawn());
+        }
+    }
+
+}
--- Laser Defender Mk2/Assets/Scripts/Scriptable Objects/EnemyWaveConfig.cs
+++ Laser Defender Mk2/Assets/Scripts/Scriptable Objects/EnemyWaveConfig.cs
@@ -9,4 +9,30 @@ public class EnemyWaveConfig : ScriptableObject
     [SerializeField] GameObject p_EnemyPrefab;
     [SerializeField] GameObject w_WaypointPrefab;
     [SerializeField] float m_TimeBetweenSpawns = 0.5f;
+    [SerializeField] float m_RandomFactor = 0.3f;
+    [SerializeField] int m_NoOfEnemies = 5;
+    [SerializeField] float m_EnemyMoveSpeed = 2f;
+
+    public GameObject GetEnemyPrefab() { return p_EnemyPrefab; }
+
+    public GameObject GetWapointPathPrefab() { return w_WaypointPrefab; }
+
+    public float GetTimeBetweenSpawn() { return m_TimeBetweenSpawns; }
+
+    public float GetSpawnRandomFactor() { return m_RandomFactor; }
+
+    public int GetNumberOfEnemies() { return m_NoOfEnemies; }
+
+    public float GetEnemyMoveSpeed() { return m_EnemyMoveSpeed; }
+
+    public List<Transform> GetWaypointList()
+    {
+        var w_WaypointList = new List<Transform>();
+        foreach(Transform child in w_WaypointPrefab.transform)
+        {
+            w_WaypointList.Add(child);
+        }
+
+        return w_WaypointList;
+    }
 }
