372ad6263ac59b6c3d081a4ee57d40577f9c2cf4
Brett Gilbert <brett@kalain.co.uk>
2019-03-11 21:34:46 +0000

    fdg

---
+++ TopDownGame/Assets/Sprites/Characters/Scripts/CharacterMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    float speed = 6f;
+    Vector2 targetPosition;
+    Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            targetPosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+           // target.position = targetPosition;
+        }
+
+        if((Vector2)transform.position != targetPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+    }
+}
