37dcc849b8abd394d1be1af40a794e0c6caa0289
Brett Gilbert <brett@kalain.co.uk>
2019-03-11 21:04:57 +0000

    jhk

---
+++ Brett's Self Learning/Assets/Game Types/2DMouseClick/Scripts/MovePlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    private float characterSpeed = 4.0f;
+    private Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            RaycastHit2D mouseClickLocation = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.zero);
+
+            targetPosition = mouseClickLocation.transform.position;
+            // targetLocation.position = targetPosition;
+            Debug.Log("Mouse button pressed " + targetPosition.x + " and y: " + targetPosition.y);
+        }
+
+        if((Vector3)transform.position != targetPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, characterSpeed * Time.deltaTime);
+            Debug.Log("object: " + transform.position.x + " and y: " + transform.position.y);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
