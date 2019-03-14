af86ded9a9f4c0ccd827a9cde675cee63d383a93
Brett Gilbert <brett@kalain.co.uk>
2019-03-14 18:14:44 +0000

    n
    n

--- TopDownGame/Assets/Sprites/Characters/Scripts/CharacterMove.cs
+++ TopDownGame/Assets/Sprites/Characters/Scripts/CharacterMove.cs
@@ -7,6 +7,12 @@ public class CharacterMove : MonoBehaviour
     float speed = 6f;
     Vector2 targetPosition;
     Transform target;
+    float padding = 1f;
+
+    float xMin, xMax, yMin, yMax;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +32,17 @@ public class CharacterMove : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
         }
+    }
+
+    // Methods
+
+    private void SetupMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 }
