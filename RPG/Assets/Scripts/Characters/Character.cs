f4308f220b0728a855c511068cafd3f9dc326159
Kalain <brett@kalain.co.uk>
2019-07-08 20:01:59 +0100

    Just adding my RPG

---
+++ RPG/Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    // Specifiy Variables for use within this class
+
+    // Set the speed of the character
+    [SerializeField] private float speed;
+
+    // Player Direction Refernece
+    protected Vector2 direction;
+
+    // Reference to the Animator Class
+    private Animator animator;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame.  Marked as Virtual so it can be overriden by a subclass.
+    protected virtual void Update()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// Move The Player
+    /// </summary>
+    public void Move()
+    {
+        // Makes sure that the player moves.
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        // Checks if we are moving or standing still.  If move, play the move animation
+        if(direction.x !=0 || direction.y !=0)
+        {
+            // Animate the players movement
+            AnimateMovement(direction);
+        }
+        else
+        {
+            // go back to idle animation if we are not moving.
+            animator.SetLayerWeight(1, 0);
+        }
+    }
+
+    /// <summary>
+    /// Makes the player animate in the correct direction
+    /// </summary>
+    /// <param name="direction"></param>
+    public void AnimateMovement(Vector2 direction)
+    {
+        animator.SetLayerWeight(1, 1);
+
+        //Set the animation parameter so that he faces the correct direction.
+        animator.SetFloat("x", direction.x);
+        animator.SetFloat("y", direction.y);
+    }
+}
---
+++ RPG/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    [SerializeField] private Stat playerHealth;
+    [SerializeField] private Stat playerMana;
+
+   // [SerializeField] private float healthValue;
+
+    [SerializeField] private float initHealth;
+    [SerializeField] private float initMana;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        playerHealth.Initialise(initHealth, initHealth);
+        playerMana.Initialise(initMana, initMana);
+
+        base.Start();
+    }
+
+    // Update is called once per frame - We are overriding the character.cs update function
+    protected override void Update()
+    {
+
+        // Executes the GetUserInput function
+        GetUserInput();
+        base.Update();
+    }
+
+    /// <summary>
+    /// Listen's to the players input.
+    /// </summary>
+    private void GetUserInput()
+    {
+        direction = Vector2.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            playerHealth.MyCurrentValue -= 10;
+            playerMana.MyCurrentValue -= 10;
+        }
+
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            playerHealth.MyCurrentValue += 10;
+            playerMana.MyCurrentValue += 10;
+        }
+    }
+}
